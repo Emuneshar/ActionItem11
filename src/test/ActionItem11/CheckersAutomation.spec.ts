aac2424d235660dcf5a5fe076c063815bd8f01f0
Eshwar Muneshar <60401349+Emuneshar@users.noreply.github.com>
2025-01-02 19:10:25 -0500

    added code for second piece to move

--- src/test/ActionItem11/CheckersAutomation.spec.ts
+++ src/test/ActionItem11/CheckersAutomation.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect, Page} from "@playwright/test"
-import { verifyTitle } from "../../main/Reusable_Methods"
+import { click, verifyTitle } from "../../main/Reusable_Methods"
 
 let page: Page
 
@@ -10,11 +10,21 @@ test.beforeAll(async ({browser}) => {
 
 const url = "https://www.gamesforthebrain.com/game/checkers/"
 const titleForVerification = "Checkers - Games for the Brain"
+const xpathFirstPiece = "//*[@name = 'space62']"
+const xpathFirstPieceTo = "//*[@name = 'space53']"
+const xpathSecondPiece = "//*[@name = 'space42']"
+const xpathSecondPieceTo = "//*[@name = 'space33']"
 
 // Beginning of test case
 test("Checkers Challenge", async() => {
   await page.goto(url)
   await verifyTitle(page, titleForVerification)
-
+  await click(page, xpathFirstPiece, "First Piece")
+  await click(page, xpathFirstPieceTo, "First piece went successfully")
+  await page.waitForTimeout(3000)
+  await click(page, xpathSecondPiece, "Second Piece highlighed")
+  await page.waitForTimeout(3000)
+  await click(page, xpathSecondPieceTo, "Second Piece Moved successfully")
+  await page.waitForTimeout(3000)
 
 })
